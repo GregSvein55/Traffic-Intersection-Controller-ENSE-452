c3d5c37d77990b1f2a29487c553bb393a5c43ef5
GregSvein55 <90286659+GregSvein55@users.noreply.github.com>
2022-11-18 16:35:32 -0600

    Update main.c

--- 452Lab3new/Core/Src/main.c
+++ 452Lab3new/Core/Src/main.c
@@ -149,107 +149,107 @@ int main(void)
   {
 
 
-	  	  //adding and updating info at top
-	      HAL_UART_Transmit(&huart2, "\x1b[s", 3, 1000);
-	 	  HAL_UART_Transmit(&huart2, "\x1b[3;0f", 7, 1000);
-	 	  resetInput();
-	 	  strcpy((char *) inputTXBuffer, "Number of commands: ");
-	 	  HAL_UART_Transmit(&huart2, inputTXBuffer, strlen((char *) inputTXBuffer), 1000);
-	 	  sprintf(input, "%d", commands);
-	 	  HAL_UART_Transmit(&huart2, input, strlen((char *) input), 1000);
-	 	  HAL_UART_Transmit(&huart2, "\x1b[u", 3, 1000);
-	 	  HAL_Delay(1000);
-	 	  commands += 1;
+	  //adding and updating info at top
+	  HAL_UART_Transmit(&huart2, "\x1b[s", 3, 1000);
+	  HAL_UART_Transmit(&huart2, "\x1b[3;0f", 7, 1000);
+	  resetInput();
+	  strcpy((char *) inputTXBuffer, "Number of commands: ");
+	  HAL_UART_Transmit(&huart2, inputTXBuffer, strlen((char *) inputTXBuffer), 1000);
+	  sprintf(input, "%d", commands);
+	  HAL_UART_Transmit(&huart2, input, strlen((char *) input), 1000);
+	  HAL_UART_Transmit(&huart2, "\x1b[u", 3, 1000);
+	  HAL_Delay(1000);
+	  commands += 1;
 
 
-	 	  //tasks from Lab 2, takes user inputs and completes the commands
+	  //tasks from Lab 2, takes user inputs and completes the commands
 
-	 	  x = 0;
+	  x = 0;
 
-	 		  strcpy((char *)cliBufferTX, "\nEnter a command for the LED:\r\n");
-	 		  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
+		  strcpy((char *)cliBufferTX, "\nEnter a command for the LED:\r\n");
+		  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
 
-	 		  while(cliBufferTX[0] != '\r')
-	 		  {
-	 			  if(HAL_UART_Receive(&huart2, cliBufferRX, 1, 300) == HAL_OK)
-	 			  {
-	 				  if(cliBufferRX[0] == '\b')
-	 				  {
-	 					  cliBufferTX[0] = cliBufferRX[0];
-	 					  cliBufferTX[1] = ' ';
-	 					  cliBufferTX[2] = '\b';
-	 					  HAL_UART_Transmit(&huart2, cliBufferTX, 3, 1000);
-	 					  x--;
-	 				  }
-	 				  else
-	 				  {
-	 					  cliBufferTX[0] = cliBufferRX[0];
-	 					  input[x] = cliBufferRX[0];
-	 					  HAL_UART_Transmit(&huart2, cliBufferTX, 1, 1000);
-	 					  x++;
-	 				  }
-	 			  }
-	 		  }
+		  while(cliBufferTX[0] != '\r')
+		  {
+			  if(HAL_UART_Receive(&huart2, cliBufferRX, 1, 300) == HAL_OK)
+			  {
+				  if(cliBufferRX[0] == '\b')
+				  {
+					  cliBufferTX[0] = cliBufferRX[0];
+					  cliBufferTX[1] = ' ';
+					  cliBufferTX[2] = '\b';
+					  HAL_UART_Transmit(&huart2, cliBufferTX, 3, 1000);
+					  x--;
+				  }
+				  else
+				  {
+					  cliBufferTX[0] = cliBufferRX[0];
+					  input[x] = cliBufferRX[0];
+					  HAL_UART_Transmit(&huart2, cliBufferTX, 1, 1000);
+					  x++;
+				  }
+			  }
+		  }
 
-	 		  if(input[0] == 'h' && input[1] == 'e' && input[2] == 'l' && input[3] == 'p' && input[4] == '\r')
-	 		  {
-	 			  strcpy((char *)cliBufferTX, "\non -> turns the green LED on\r\n");
-	 			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
-	 			  strcpy((char *)cliBufferTX, "\noff -> turns the green LED off\r\n");
-	 			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
-	 			  strcpy((char *)cliBufferTX, "\nquery -> returns the state of the green LED\r\n");
-	 			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
+		  if(input[0] == 'h' && input[1] == 'e' && input[2] == 'l' && input[3] == 'p' && input[4] == '\r')
+		  {
+			  strcpy((char *)cliBufferTX, "\non -> turns the green LED on\r\n");
+			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
+			  strcpy((char *)cliBufferTX, "\noff -> turns the green LED off\r\n");
+			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
+			  strcpy((char *)cliBufferTX, "\nquery -> returns the state of the green LED\r\n");
+			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
 
-	 		  }
-	 		  else if(input[0] == 'o' && input[1] == 'n' && input[2] == '\r')
-	 		  {
-	 			  if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) == 1)
-	 			  {
-	 				  strcpy((char *)cliBufferTX, "\nThe LED is already on!\r\n");
-	 			  }
-	 			  else
-	 			  {
-	 				  strcpy((char *)cliBufferTX, "\nDone!\r\n");
-	 			  }
-	 			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
-	 			  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+		  }
+		  else if(input[0] == 'o' && input[1] == 'n' && input[2] == '\r')
+		  {
+			  if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) == 1)
+			  {
+				  strcpy((char *)cliBufferTX, "\nThe LED is already on!\r\n");
+			  }
+			  else
+			  {
+				  strcpy((char *)cliBufferTX, "\nDone!\r\n");
+			  }
+			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
+			  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
 
 
-	 		  }
-	 		  else if(input[0] == 'o' && input[1] == 'f' && input[2] == 'f' && input[3] == '\r')
-	 		  {
-	 			  if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) == 1)
-	 			  {
-	 				  strcpy((char *)cliBufferTX, "\nDone!\r\n");
-	 			  }
-	 			  else
-	 			  {
-	 				  strcpy((char *)cliBufferTX, "\nThe LED is already off!\r\n");
-	 			  }
-	 			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
-	 			  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-	 		  }
-	 		  else if(input[0] == 'q' && input[1] == 'u' && input[2] == 'e' && input[3] == 'r' && input[4] == 'y' && input[5] == '\r')
-	 		  {
-	 			  if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) == 1)
-	 			  {
-	 				  strcpy((char *)cliBufferTX, "\nThe LED is on!\r\n");
-	 			  }
-	 			  else
-	 			  {
-	 				  strcpy((char *)cliBufferTX, "\nThe LED is off!\r\n");
-	 			  }
-	 			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
-	 		  }
-	 		  else if(input[0] == '\r')
-	 		  {
-	 			  continue;
-	 		  }
-	 		  else
-	 		  {
-	 			  strcpy((char *)cliBufferTX, "\nError. Try again.\r\n");
-	 			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
-	 		  }
+		  }
+		  else if(input[0] == 'o' && input[1] == 'f' && input[2] == 'f' && input[3] == '\r')
+		  {
+			  if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) == 1)
+			  {
+				  strcpy((char *)cliBufferTX, "\nDone!\r\n");
+			  }
+			  else
+			  {
+				  strcpy((char *)cliBufferTX, "\nThe LED is already off!\r\n");
+			  }
+			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
+			  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+		  }
+		  else if(input[0] == 'q' && input[1] == 'u' && input[2] == 'e' && input[3] == 'r' && input[4] == 'y' && input[5] == '\r')
+		  {
+			  if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) == 1)
+			  {
+				  strcpy((char *)cliBufferTX, "\nThe LED is on!\r\n");
+			  }
+			  else
+			  {
+				  strcpy((char *)cliBufferTX, "\nThe LED is off!\r\n");
+			  }
+			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
+		  }
+		  else if(input[0] == '\r')
+		  {
+			  continue;
+		  }
+		  else
+		  {
+			  strcpy((char *)cliBufferTX, "\nError. Try again.\r\n");
+			  HAL_UART_Transmit(&huart2, cliBufferTX, strlen((char *)cliBufferTX), 1000);
+		  }
 
 	 	 
     /* USER CODE BEGIN 3 */
